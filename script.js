48957a6dda025b29449783f7528bd02b6590d3df
paulo-valim <plvalim@gmail.com>
2022-04-11 14:51:36 +0200

    finished UI

--- script.js
+++ script.js
@@ -1,90 +1,155 @@
-//The game presents 3 choices of “weapons” for the user to play the game:
 let rock = "rock";
 let paper = "paper";
 let scissors = "scissors";
+
 let weapons = [rock, paper, scissors];
 
-//The computer randomly choose one option between Rock, Paper, and Scissors;
 function computerPlay() {
-  let computerWeapon = weapons[parseInt(Math.random() * 3)];
-  return computerWeapon;
+  let computerWeapon;
+  return (computerWeapon = weapons[parseInt(Math.random() * 3)]);
 }
 
-//The user choose one option between Rock, Paper, and Scissors;
-function userPlay() {
-  let userInput = prompt("Your move: Rock, Paper or Scissors");
-  let userWeapom = userInput.toLowerCase();
+let weapon = document.querySelectorAll(".weaponbtn");
+weapon.forEach((weaponbtn) => {
+  weaponbtn.addEventListener("click", userWeapon);
+});
 
-  if (userWeapom == rock || userWeapom == paper || userWeapom == scissors) {
-    return userWeapom;
-  } else {
-    alert(
-      "The only weapons allowed are: Rock, Paper or Scissors, choose one of them! ;)"
-    );
-  }
+function userWeapon(e) {
+  let userPlay;
+  userPlay = e.target.getAttribute("data-weapon");
+  e.target.classList.add("playing");
+  gameRound(userPlay, computerPlay());
 }
 
-//Compare the choices made by the user and the computer and display to the user the result of the round;
-//Count 1 point for each victory
+let turnOff = document.querySelectorAll(".weaponbtn");
+turnOff.forEach((weaponbtn) =>
+  weaponbtn.addEventListener("transitionend", removeTransition)
+);
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
 
 let computerScore = 0;
 let playerScore = 0;
 
 function gameRound(userPlay, computerPlay) {
-  console.log(computerPlay);
-  console.log(userPlay);
-  let computerWin =
+  printWeaponChoices(userPlay, computerPlay);
+  let result;
+  if (
     (userPlay == rock && computerPlay == paper) ||
     (userPlay == paper && computerPlay == scissors) ||
-    (userPlay == scissors && computerPlay == rock);
-  let playerWin =
+    (userPlay == scissors && computerPlay == rock)
+  ) {
+    result = "computerWin";
+  } else if (
     (userPlay == rock && computerPlay == scissors) ||
     (userPlay == paper && computerPlay == rock) ||
-    (userPlay == scissors && computerPlay == paper);
-  let draw = userPlay == computerPlay;
-  let result;
+    (userPlay == scissors && computerPlay == paper)
+  ) {
+    result = "playerWin";
+  } else if (userPlay == computerPlay) {
+    result = "draw";
+  }
+  gameRoundResult(result, userPlay, computerPlay);
+}
 
+function gameRoundResult(gameRound, userPlay, computerPlay) {
   let playerBigLetter = userPlay.charAt(0).toUpperCase() + userPlay.slice(1);
   let computerBigLetter =
     computerPlay.charAt(0).toUpperCase() + computerPlay.slice(1);
 
-  if (computerWin) {
-    alert("You lose! " + computerBigLetter + " beats " + userPlay + "!");
+  let printMatchResult = document.querySelector(".match-result");
+  let printMatchComment = document.querySelector(".match-comment");
+
+  if (gameRound === "computerWin") {
+    printMatchResult.textContent = "You Lost!";
+    printMatchComment.textContent =
+      computerBigLetter + " beats " + userPlay + "!";
     computerScore += 1;
-  } else if (playerWin) {
-    alert("You win! " + playerBigLetter + " beats " + computerPlay + "!");
+  } else if (gameRound === "playerWin") {
+    printMatchResult.textContent = "You Won!";
+    printMatchComment.textContent =
+      playerBigLetter + " beats " + computerPlay + "!";
     playerScore += 1;
-  } else if (draw) {
-    alert("Draw! Please, be better!");
+  } else if (gameRound === "draw") {
+    printMatchResult.textContent = "It's a Draw!";
+    printMatchComment.textContent =
+      playerBigLetter + " ties with " + computerPlay + "!";
   }
+
+  printMatchScore(playerScore, computerScore);
+  gameScore(playerScore, computerScore);
+}
+
+function printWeaponChoices(userPlay, computerPlay) {
+  let printUserPlay = document.querySelector(".user-play");
+  if (userPlay === "rock") {
+    printUserPlay.textContent = "👊";
+  } else if (userPlay === "paper") {
+    printUserPlay.textContent = "🤚";
+  } else if (userPlay === "scissors") {
+    printUserPlay.textContent = "🖖";
+  }
+  let printComputerPlay = document.querySelector(".computer-play");
+  if (computerPlay === "rock") {
+    printComputerPlay.textContent = "👊";
+  } else if (computerPlay === "paper") {
+    printComputerPlay.textContent = "🤚";
+  } else if (computerPlay === "scissors") {
+    printComputerPlay.textContent = "🖖";
+  }
+  console.log(computerPlay);
+  console.log(userPlay);
+}
+
+function printMatchScore(playerScore, computerScore) {
+  let printUserScore = document.querySelector(".user-score");
+  printUserScore.textContent = " " + playerScore;
+  let printComputerScore = document.querySelector(".computer-score");
+  printComputerScore.textContent = " " + computerScore;
   console.log(computerScore);
   console.log(playerScore);
 }
 
-gameRound(userPlay(), computerPlay());
-
-// the first to reach 5 victories is the Winner!
-// If the user reaches 5 wins first, display a message: You won!
-// If the computer reaches 5 wins first, display a message: You Lose!
-
-function game() {
-  for (let i = 0; computerScore < 5 && playerScore < 5; i++) {
-    {
-      alert("Human = " + playerScore + " & Machine = " + computerScore);
-      gameRound(userPlay(), computerPlay());
-    }
+function gameScore(playerScore, computerScore) {
+  let modal = document.querySelector(".modal");
+  let score = document.querySelector(".modal-title");
+  let overlay = document.querySelector(".overlay");
+  if (computerScore === 5) {
+    score.innerHTML =
+      "You lost! <br/> You: " + playerScore + " x Computer: " + computerScore;
+    modal.style.display = "flex";
+    overlay.style.display = "block";
+  } else if (playerScore === 5) {
+    score.innerHTML =
+      "You won! <br/> You: " + playerScore + " x Machine: " + computerScore;
+    modal.style.display = "flex";
+    overlay.style.display = "block";
   }
-  if (computerScore == 5) {
-    alert(
-      "The machines won! Human = " +
-        playerScore +
-        " & Machine = " +
-        computerScore
-    );
-  } else if (playerScore == 5)
-    alert(
-      "The humans won! Human = " + playerScore + " & Machine = " + computerScore
-    );
 }
 
-game();
+let restart = document.querySelector(".btn-restart");
+restart.addEventListener("click", restartGame);
+
+function restartGame() {
+  let modal = document.querySelector(".modal");
+  let overlay = document.querySelector(".overlay");
+  let restartTitle = document.querySelector(".match-result");
+  let restartInstruction = document.querySelector(".match-comment");
+  let restartUserScore = document.querySelector(".user-score");
+  let restartComputerScore = document.querySelector(".computer-score");
+  let restartUserPlay = document.querySelector(".user-play");
+  let restartComputerPlay = document.querySelector(".computer-play");
+  modal.style.display = "none";
+  overlay.style.display = "none";
+  restartTitle.textContent = "Choose your weapon:";
+  restartInstruction.textContent = "First to score 5 points wins the game!";
+  playerScore = 0;
+  computerScore = 0;
+  restartUserScore.textContent = " 0";
+  restartComputerScore.textContent = " 0";
+  restartUserPlay.textContent = "?";
+  restartComputerPlay.textContent = "?";
+}
